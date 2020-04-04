6fccec00dd026a1dbe872a368098443d248f521e
ddiv739 <ddiv739@aucklanduni.ac.nz>
2020-04-05 02:05:25 +1200

    basic chat functionality working. Note: IP hardcoded

--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
 import time
 from flask import Flask
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/time')
 def get_current_time():
@@ -10,7 +15,20 @@ def get_current_time():
 # ----------------------- v1
 
 # Emit message to everyone (broadcast)
+@socketio.on('add message event')
+def add_message(message):
+    print(message)
+    emit('message', message, broadcast=True)
 
+@socketio.on('connect')
+def connect():
+    emit('conn resp', {'data': 'Connected'})    
 
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
 
+if __name__ == '__main__':
+    # socketio.run(app)
+    socketio.run(app, host="0.0.0.0")
 
--- src/App.js
+++ src/App.js
@@ -1,35 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client'
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+var socket
+class App extends Component {
 
-  useEffect(() => {
-    fetch("/time").then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+  constructor() {
+    super();
+    this.state = {
+      endpoint : "http://192.168.1.151:5000",
+      messages : [],
+      currentTime: 0,
+      new_message : ""
+    }
+    socket = socketIOClient(this.state.endpoint)
+    this.clickHandle = this.clickHandle.bind(this)
+  }
+
+  componentDidMount() {
+    socket.emit('add message event','yo it worked')
+
+    socket.on('message', (msg) => {
+      console.log(msg);
+      this.setState((prevState) => ({
+        messages: [...prevState.messages,msg]
+      }))
+    })
+    //replace with fetching messages
+    fetch('/time').then(res => res.json()).then(data => {
+      this.setState({currentTime: data.time})
     });
-  }, []);
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
-  );
+  clickHandle() {
+    socket.emit('add message event', this.state.new_message)
+    this.setState({new_message:''})
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <p>The current time is {this.state.currentTime}.</p>
+          <ul>
+            {
+              this.state.messages.map((message) => 
+                <li key={message}>
+                  {message}
+                </li>
+              )
+            }
+          </ul>
+        
+          <input value={this.state.new_message} name="new_message" onChange={e => this.setState({new_message:e.target.value})} />
+          <button onClick={this.clickHandle}>Send Message</button>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
